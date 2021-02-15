919484a2e2155141ecb0be1d7d7062eb29e4ca00
ferdigix <ferdinand.leonardi@gmail.com>
2021-02-15 07:10:45 -0800

    pushing jenkins php

---
+++ jenkinsidk.php
@@ -0,0 +1,22 @@
+<?php
+
+$link = new mysqli("172.19.0.2","root","uas","Trucorp");
+if (!$link){
+echo "Tidak bisa connect ke MySQL" . PHP_EOL;
+exit;
+}
+$sql = "SELECT * FROM users";
+$result = $link->query($sql);
+
+if($result){
+$count = 0;
+while($row = $result->fetch_assoc()){
+$count = $count + 1;
+}
+}
+$link->close();
+
+
+?>
+
+
